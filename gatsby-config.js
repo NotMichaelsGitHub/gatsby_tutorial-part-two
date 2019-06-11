1f40dff32e4cd691ce142269d96ba4b1ec372ae9
NotMichaelsGitHub <notdipaolo@outlook.com>
2019-06-11 03:20:19 -0500

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+}
+
+{
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+  ]
+}
+
+{
+  plugins: [
+      {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+              ...
+          }
+      },
+      'gatsby-plugin-offline'
+  ]
+}
+
+{
+  plugins: [`gatsby-plugin-react-helmet`]
+}
